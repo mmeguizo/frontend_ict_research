a2a924cba477c336fff26511316f0e74b4f7690f
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2026-09-14 15:35:35 +0800

    update

--- src/app/core/config/environment.ts
+++ src/app/core/config/environment.ts
@@ -14,7 +14,8 @@ export interface Environment {
 
 export const environment: Environment = {
   production: false,
-  apiUrl: 'http://localhost:4000/graphql',
+  apiUrl: 'https://backend-ict-research.onrender.com',
+  // apiUrl: 'http://localhost:4000/graphql',
   wsUrl: 'ws://localhost:4000/graphql',
   google: {
     clientId: '1081954751485-a4s2spmm54c954e1ejsu179dieca71ii.apps.googleusercontent.com',
